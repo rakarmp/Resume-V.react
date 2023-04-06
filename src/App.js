20cdbe3836ef4d6e5d9b230c93a0d0f8a9293e08
rakarmp <raka_088@smkassalaambandung.sch.id>
2023-04-06 16:22:40 -0700

    First Start

---
+++ src/App.js
@@ -0,0 +1,11 @@
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1>Test For Resume In React</h1>
+      </header>
+    </div>
+  );
+}
+
+export default App;

df8100dcaf50e4deaa5f021825acf11c46f2a3ae
rakarmp <raka_088@smkassalaambandung.sch.id>
2023-04-06 18:06:54 -0700

    +skills

---
+++ src/components/Skills.js
@@ -0,0 +1,88 @@
+import React from "react";
+
+function Skills() {
+  return (
+    <section className="pb-4 mt-4 first:mt-0">
+      <div className="break-inside-avoid">
+        <h2 className="mb-2 text-lg font-bold tracking-widest text-gray-700 print:font-normal">
+          SKILLS
+        </h2>
+        <section className="mb-2 break-inside-avoid">
+          <header>
+            <h3 className="text-lg font-semibold text-gray-700 leading-snugish">
+              Dev
+            </h3>
+          </header>
+
+          <div className="my-1 last:pb-1">
+            <ul className="flex flex-wrap text-sm2 leading-relaxed -mr-1.6 -mb-1">
+              <li className="px-2.5 mr-1.6 mb-1 text-gray-200 leading-relaxed print:bg-white print:border-inset bg-gray-250">
+                HTML5
+              </li>
+              <li className="px-2.5 mr-1.6 mb-1 text-gray-200 leading-relaxed print:bg-white print:border-inset bg-gray-250">
+                CSS3
+              </li>
+              <li className="px-2.5 mr-1.6 mb-1 text-gray-200 leading-relaxed print:bg-white print:border-inset bg-gray-250">
+                SCSS
+              </li>
+              <li className="px-2.5 mr-1.6 mb-1 text-gray-200 leading-relaxed print:bg-white print:border-inset bg-gray-250">
+                Javascript
+              </li>
+              <li className="px-2.5 mr-1.6 mb-1 text-gray-200 leading-relaxed print:bg-white print:border-inset bg-gray-250">
+                JSX
+              </li>
+              <li className="px-2.5 mr-1.6 mb-1 text-gray-200 leading-relaxed print:bg-white print:border-inset bg-gray-250">
+                React
+              </li>
+              <li className="px-2.5 mr-1.6 mb-1 text-gray-200 leading-relaxed print:bg-white print:border-inset bg-gray-250">
+                React-Router
+              </li>
+
+              <li className="px-2.5 mr-1.6 mb-1 text-gray-200 leading-relaxed print:bg-white print:border-inset bg-gray-250">
+                Next.js
+              </li>
+              <li className="px-2.5 mr-1.6 mb-1 text-gray-200 leading-relaxed print:bg-white print:border-inset bg-gray-250">
+                MERN
+              </li>
+              <li className="px-2.5 mr-1.6 mb-1 text-gray-200 leading-relaxed print:bg-white print:border-inset bg-gray-250">
+                PHP
+              </li>
+              <li className="px-2.5 mr-1.6 mb-1 text-gray-200 leading-relaxed print:bg-white print:border-inset bg-gray-250">
+                Laravel
+              </li>
+              <li className="px-2.5 mr-1.6 mb-1 text-gray-200 leading-relaxed print:bg-white print:border-inset bg-gray-250">
+                Flutter
+              </li>
+            </ul>
+          </div>
+        </section>
+
+        <section className="mb-2 break-inside-avoid">
+          <header>
+            <h3 className="font-semibold text-gray-700 text-m leading-snugish">
+              DevOps
+            </h3>
+          </header>
+          <div className="my-1 last:pb-1">
+            <ul className="flex flex-wrap text-sm2 leading-relaxed -mr-1.6 -mb-1">
+              <li className="px-2.5 mr-1.6 mb-1 text-gray-200 leading-relaxed print:bg-white print:border-inset bg-gray-250">
+                Version Control
+              </li>
+              <li className="px-2.5 mr-1.6 mb-1 text-gray-200 leading-relaxed print:bg-white print:border-inset bg-gray-250">
+                Git
+              </li>
+              <li className="px-2.5 mr-1.6 mb-1 text-gray-200 leading-relaxed print:bg-white print:border-inset bg-gray-250">
+                Firebase
+              </li>
+              <li className="px-2.5 mr-1.6 mb-1 text-gray-200 leading-relaxed print:bg-white print:border-inset bg-gray-250">
+                Linux
+              </li>
+            </ul>
+          </div>
+        </section>
+      </div>
+    </section>
+  );
+}
+
+export default Skills;

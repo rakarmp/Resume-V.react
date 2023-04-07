caa7d6286699ce1c207881c93d3910e90a519ef9
rakarmp <raka_088@smkassalaambandung.sch.id>
2023-04-06 18:07:02 -0700

    +summary

---
+++ src/components/Summary.js
@@ -0,0 +1,23 @@
+import React from "react";
+
+function Summary() {
+  return (
+    <section className="pb-2 pb-4 mt-4 border-b-4 first:mt-0">
+      <div className="break-inside-avoid">
+        <h2 className="mb-2 text-lg font-bold tracking-widest text-gray-700 print:font-normal">
+          SUMMARY
+        </h2>
+
+        <section className="mb-2 break-inside-avoid">
+          <p className="mt-2 leading-normal text-gray-700 text-md">
+            Web developer fresh graduate,Memiliki pengetahuan dan keterampilan
+            yang cukup dalam bidang web development,Terutama dalam pengembangan
+            sebuah website
+          </p>
+        </section>
+      </div>
+    </section>
+  );
+}
+
+export default Summary;
